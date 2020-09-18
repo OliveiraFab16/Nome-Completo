af57ebc383956fd4385984928e912edb458bd7cb
oliveiraFab16 <fabriciocarvalho932@gmail.com>
2020-09-18 16:13:25 -0300

    sobrenome

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Nome_Completo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string nome;
+            string sobrenome;
+
+            //<---Inicio(Nome e Sobrenome)--->//
+            Console.Write("Digite Seu Nome: ");
+            nome = Console.ReadLine();
+            Console.Write("Digite Seu Sobrenome: ");
+            sobrenome = Console.ReadLine();
+            Console.Write("\n");
+            //<---Fim(Nome e Sobrenome)--->//
+
+            //<---Inicio(Nome Completo)--->//
+            Console.Write($"Nome Completo: {nome} {sobrenome}");
+            Console.Write("\n");
+            //<---Fim(Nome Completo)--->//
+
+            //<---Inicio(Nome de Catálogo)--->//
+            Console.WriteLine($"Nome de Catálogo: {sobrenome.ToUpper()}, {nome}");
+            Console.Write("\n");
+            //<---Fim(Nome de Catálogo)--->//
+
+            //<---Inicio(Sair)--->//
+            Console.WriteLine("Pressione ENTER para sair");
+            Console.ReadKey();
+            //<---Fim(Sair)--->//
+        }
+    }
+}
